835df79329d6dd6740df9a935e32b6bcc6852863
Jamil Salim Daruix <jamilsd@gmail.com>
2023-02-07 22:10:58 -0300

    Aula dois

---
+++ Sln-Semana-02-POO/Semana-02-POO/CentroDeCusto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_02_POO
+{
+    /// Aqui criou uma classe
+    /// Foi Criado um Enpacsulamento
+    public class CentroDeCusto
+    {
+        /// Criação de propriedades públicas
+        public int Id { get; set; } // Default 0
+        public string Descricao { get; set; } // Null 
+        public DateTime DataCriacao { get; set; } // 01/01/0001 00:00:00
+        public decimal VerbaLiberada { get; set; } // 0.0
+        public int AnoVerbaLiberada { get; set; } // = 0
+
+        //Variavél privada
+        private static decimal custoReal;
+
+        //Construtor Statico
+        static CentroDeCusto()
+        {
+            custoReal = 1500M;
+        }
+
+        // Construtor com parametros
+        public CentroDeCusto(int id, 
+                             string descricao, 
+                             DateTime dataCriacao,
+                             decimal verbaLiberada, 
+                             int anoVerbaLiberada)
+        {
+            /*
+             * Preencher as propriedades com os 
+             * parametros recebidos do construtor
+             */
+            Id = id;
+            Descricao = descricao;
+            DataCriacao = dataCriacao;
+            VerbaLiberada = verbaLiberada;
+            AnoVerbaLiberada = anoVerbaLiberada;
+        }
+
+        //Novo Método que vai preencher a variavel 
+        //privada chamda custoReal com o valor do parametro recebido (custoRealNovo)
+        public void CalcularCustoReal(decimal custoRealNovo)
+        {
+            custoReal = custoRealNovo;
+
+            Console.WriteLine($"O novo custo real do centro de custo {Descricao}, com o id {Id} é {custoReal}");
+        }
+    }
+}
--- Sln-Semana-02-POO/Semana-02-POO/Program.cs
+++ Sln-Semana-02-POO/Semana-02-POO/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Semana_02_POO;
+
 Console.WriteLine("Hello, World!");
+
+//Declaro uma variavel com instancia da minha classe
+CentroDeCusto nomeVariavel = new CentroDeCusto(5958, "Teste", DateTime.Now, 1500.0M, 2023);
+nomeVariavel.CalcularCustoReal(10);
